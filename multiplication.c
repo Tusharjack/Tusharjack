e8b2b81415f0cb34a2030c5f3bc703a34715c9a1
Tusharjack <115859396+Tusharjack@users.noreply.github.com>
2022-10-17 20:21:44 +0530

    Add files via upload

---
+++ multiplication.c
@@ -0,0 +1,17 @@
+ #include <stdio.h>
+ #include <conio.h>
+ int main()
+ clrscr();
+ {
+ 	int a,b,c ;
+ printf ("value of a is : \n");
+scanf  ("%d",& a);
+  printf ("value of b is : \n");
+  scanf  ("%d",& b);
+  printf  ("multiplication of a*b = %d ",c);
+  getch ();
+  return 0 ;
+  
+  
+ }
+ 
